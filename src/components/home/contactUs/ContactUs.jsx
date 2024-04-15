14fb0702f377967ae206b58b5061a6de8da12e56
Soumya-0x000 <soumyadas429@gmail.com>
2024-04-15 08:57:51 +0530

    contact us page is done

--- src/components/home/contactUs/ContactUs.jsx
+++ src/components/home/contactUs/ContactUs.jsx
@@ -69,97 +69,99 @@ const ContactUs = () => {
             </div>
 
             {/* content */}
-            <div className='relative flex flex-col items-center justify-center text-blue-900 mx-6 mt-16 mb-8 text-2xl font-bold text-center capitalize font-onest gap-y-2 md:text-4xl'>
-                We are located here!
-            </div>
+            <>
+                <div className='relative flex flex-col items-center justify-center text-blue-900 mx-6 mt-16 mb-8 text-2xl font-bold text-center capitalize font-onest gap-y-2 md:text-4xl'>
+                    We are located here!
+                </div>
 
-            <div className=' flex items-center justify-center w-full px-3'>
-                <div className='shadow contentContainer'>
-                    {/* map */}
-                    <div className='row mapContainer'>
-                        <iframe src={map} className='map'></iframe>
-                    </div>
-
-                    {/* form */}
-                    <div className='formContainer space-y-16'>
-                        <div className=' space-y-2 pl-3'>
-                            <div className='text-3xl font-mavenPro font-bold text-blue-900'>
-                            Contact us
-                            </div>
-                            <div className='text-lg font-mavenPro font-bold text-indigo-500'>
-                                We're 24x7 open for any suggestion or having a chat
-                            </div>
+                <div className=' flex items-center justify-center w-full px-3'>
+                    <div className='shadow contentContainer'>
+                        {/* map */}
+                        <div>
+                            <iframe src={map} className='map'></iframe>
                         </div>
 
-                        <Form onSubmit={handleSubmit} className='flex flex-col items-center justify-center h-fit overflow-auto w-fit pl-3 py-2'>
-                            <Row className="gap-y-3">
-                                <Form.Group as={Col} sm={6} controlId="formGridCountry">
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        placeholder="Full name"
-                                        required
-                                        className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.name ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
-                                        value={formData.name}
-                                        onChange={handleChange}
-                                    />
-                                </Form.Group>
+                        {/* form */}
+                        <div className='formContainer space-y-16'>
+                            <div className=' space-y-2'>
+                                <div className='text-3xl font-mavenPro font-bold text-blue-900'>
+                                Contact us
+                                </div>
+                                <div className='text-lg font-mavenPro font-bold text-indigo-500'>
+                                    We're 24x7 open for any suggestion or having a chat
+                                </div>
+                            </div>
 
-                                <Form.Group as={Col} sm={6} controlId="formGridState">
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email"
-                                        required
-                                        className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.email ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                    />
-                                </Form.Group>
-                            
-                                <Form.Group as={Col} controlId="formGridCity">
-                                    <input
-                                        type="text"
-                                        name="subject"
-                                        placeholder="Subject"
-                                        required
-                                        className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.subject ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
-                                        value={formData.subject}
-                                        onChange={handleChange}
-                                    />
-                                </Form.Group>
+                            <Form onSubmit={handleSubmit} className='flex flex-col items-center justify-center h-fit overflow-auto w-fit py-2'>
+                                <Row className="gap-y-3">
+                                    <Form.Group as={Col} sm={6} controlId="formGridCountry">
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            placeholder="Full name"
+                                            required
+                                            className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.name ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
+                                            value={formData.name}
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
 
-                                <Form.Group as={Col} xs={12} controlId="formGridAddress">
-                                    <textarea
-                                        rows={'12'}
-                                        name="message"
-                                        placeholder="Message"
-                                        required
-                                        className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.message ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
-                                        value={formData.message}
-                                        onChange={handleChange}
-                                    />
-                                </Form.Group>
-                            </Row>
-
-                            {/* submit button */}
-                            <div className="flex flex-col items-center justify-between mt-3 sm:flex-row gap-y-4 w-full">
-                                <button 
-                                className=" text-md font-bold bg-slate-800 text-cyan-300 hover:text-indigo-300 font-robotoMono ring-2 ring-violet-400 w-full sm:w-[7rem] h-9 sm:h-8 rounded-md active:ring-green-300 active:text-green-300 transition-all" 
-                                type="submit">
-                                    Register
-                                </button>
+                                    <Form.Group as={Col} sm={6} controlId="formGridState">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            placeholder="Email"
+                                            required
+                                            className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.email ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
                                 
-                                <button 
-                                className=" text-md font-bold bg-slate-800 text-cyan-300 hover:text-indigo-300 font-robotoMono ring-2 ring-violet-400 w-full sm:w-[7rem] h-9 sm:h-8 rounded-md active:ring-green-300 active:text-green-300 transition-all" 
-                                onClick={handleReset}>
-                                    Reset
-                                </button>
-                            </div> 
-                        </Form>
+                                    <Form.Group as={Col} controlId="formGridCity">
+                                        <input
+                                            type="text"
+                                            name="subject"
+                                            placeholder="Subject"
+                                            required
+                                            className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 h-full font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.subject ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
+                                            value={formData.subject}
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} xs={12} controlId="formGridAddress">
+                                        <textarea
+                                            rows={'12'}
+                                            name="message"
+                                            placeholder="Message"
+                                            required
+                                            className={`border-y-2 pt-2.5 pb-2 px-2 focus:border-b-2 transition-colors focus:outline-none bg-slate-800 font-robotoMono placeholder:text-blue-300 text-green-300 ${formData.message ? 'border-indigo-400' : ''} focus:border-indigo-400 w-full`}
+                                            value={formData.message}
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
+                                </Row>
+
+                                {/* submit button */}
+                                <div className="flex flex-col items-center justify-between mt-3 sm:flex-row gap-y-4 w-full">
+                                    <button 
+                                    className=" text-md font-bold bg-slate-800 text-cyan-300 hover:text-indigo-300 font-robotoMono ring-2 ring-violet-400 w-full sm:w-[7rem] h-9 sm:h-8 rounded-md active:ring-green-300 active:text-green-300 transition-all" 
+                                    type="submit">
+                                        Register
+                                    </button>
+                                    
+                                    <button 
+                                    className=" text-md font-bold bg-slate-800 text-cyan-300 hover:text-indigo-300 font-robotoMono ring-2 ring-violet-400 w-full sm:w-[7rem] h-9 sm:h-8 rounded-md active:ring-green-300 active:text-green-300 transition-all" 
+                                    onClick={handleReset}>
+                                        Reset
+                                    </button>
+                                </div> 
+                            </Form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </>
 
             {/* footer */}
             <div className='mt-10'>
